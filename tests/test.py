4f06bc765cfeb0f6b9ca6bcb3a9c1498fe7bf7a9
CristiMacovei <nicolae.macovei1412@stud.acs.upb.ro>
2025-08-13 21:58:40 +0300

    add test for iOS v8
    Signed-off-by: CristiMacovei <nicolae.macovei1412@stud.acs.upb.ro>

--- tests/test.py
+++ tests/test.py
@@ -100,25 +100,47 @@ def generic_test(container_name, test_name, ios_major_version, ios_version, upda
 		"/sandblaster/helpers/extract_sandbox_data.py", "-o", "/test/outputs/sb_ops", "/test/inputs/sandbox.kext", ios_version
 	])
 
-	subprocess.run([
-		"docker", "exec", run_name,
-		"/sandblaster/helpers/extract_sandbox_data.py", "-O", "/test/outputs", "/test/inputs/sandbox.kext", ios_version
-	])
+	if ios_major_version >= 9:
+		subprocess.run([
+			"docker", "exec", run_name,
+			"/sandblaster/helpers/extract_sandbox_data.py", 
+			"-O", "/test/outputs", 
+			"/test/inputs/sandbox.kext", ios_version
+		])
+	else:
+		subprocess.run([
+			"docker", "exec", run_name,
+			"mkdir", "-p", "/test/outputs/sb_profiles"
+		])
+
+		subprocess.run([
+			"docker", "exec", run_name,
+			"/sandblaster/helpers/extract_sandbox_data.py", 
+			"-O", "/test/outputs/sb_profiles", 
+			"/test/inputs/sandboxd", ios_version
+		])
 
 	subprocess.run([
 		"docker", "exec", run_name,
 		"mkdir", "/test/outputs/rev_profiles"
 	])
 
-	subprocess.run([
-		"docker", "exec", run_name,
-		"sh", "-c", f"cd /sandblaster/reverse-sandbox/ && python2.7 reverse_sandbox.py -r {ios_version} -o /test/outputs/sb_ops -d /test/outputs/rev_profiles/ /test/outputs/sandbox_bundle -psb > /test/outputs/sandbox_profiles.txt"
-	])
+	if ios_major_version >= 9:
+		subprocess.run([
+			"docker", "exec", run_name,
+			"sh", "-c", f"cd /sandblaster/reverse-sandbox/ && python2.7 reverse_sandbox.py -r {ios_version} -o /test/outputs/sb_ops -d /test/outputs/rev_profiles/ /test/outputs/sandbox_bundle -psb > /test/outputs/sandbox_profiles.txt"
+		])
 
-	subprocess.run([
-		"docker", "exec", run_name,
-		"sh", "-c", f"cd /sandblaster/reverse-sandbox/ && python2.7 reverse_sandbox.py -r {ios_version} -o /test/outputs/sb_ops -d /test/outputs/rev_profiles/ /test/outputs/sandbox_bundle"
-	])
+		subprocess.run([
+			"docker", "exec", run_name,
+			"sh", "-c", f"cd /sandblaster/reverse-sandbox/ && python2.7 reverse_sandbox.py -r {ios_version} -o /test/outputs/sb_ops -d /test/outputs/rev_profiles/ /test/outputs/sandbox_bundle"
+		])
+	else:
+		subprocess.run([
+			"docker", "exec", run_name,
+			"sh", "-c",
+			f"cd /sandblaster/reverse-sandbox/ && for i in /test/outputs/sb_profiles/*; do python2.7 reverse_sandbox.py -r {ios_version} -o /test/outputs/sb_ops -d /test/outputs/rev_profiles/ \"$i\"; done"
+		])
 
 	if update_refs:
 		subprocess.run([
@@ -152,6 +174,8 @@ def main():
 	container_name = build_image()
 
 	generic_test(container_name, "iPhone5__1_9.3_13E237", 9, ios_version="9.3")
+	
+	generic_test(container_name, "iPad2__1_8.4.1_12H321", 8, ios_version="8.4.1", update_refs=True)
 
 	remove_image(container_name)
 
